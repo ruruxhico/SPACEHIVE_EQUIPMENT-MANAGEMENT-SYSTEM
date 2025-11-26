bca898daa708186a0bc15daa27088f653b4e2239
ruruxhico <daniellejanm.15@gmail.com>
2025-11-26 17:33:23 +0800

    added routes for testing and var name changes in view_signup

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,14 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+//trial routes for Auth
+//Routes for Auth
+$routes->get('auth/login', 'Auth::login');
+$routes->post('auth/login', 'Auth::loginSubmit');
+$routes->get('auth/signup', 'Auth::signup');
+$routes->post('auth/signup', 'Auth::signupSubmit');
+$routes->get('logout', 'Auth::logout');
+$routes->get('logout', 'Users::logout');
+$routes->get('dashboard', 'Auth::index');
+$routes->get('auth/verify/(:any)', 'Auth::verify/$1');
